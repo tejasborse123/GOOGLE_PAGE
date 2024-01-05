cbc202acf0b07def59bace67268906776a938ab9
Tejas Borse <borsetejas764@gmail.com>
2024-01-05 13:14:36 +0530

    Google Search Updated

--- data.js
+++ data.js
@@ -1,44 +1,18 @@
+function handleKeyPress(event) 
+{
+    if (event.key === 'Enter') 
+    {
+        var inputValue = document.getElementById("searchInput").value;
 
-// function performGoogleSearch() {
+	    if (inputValue.trim() !== "") 
+	    {
 
-//     var inputValue = document.getElementById("searchInput").value;
-
-//     if (inputValue.trim() !== "") 
-//     {
-//         var googleSearchURL = "https://www.google.com/search?q=" + encodeURIComponent(inputValue);
-//         window.open(googleSearchURL, "_blank");
-//     } 
-//     else 
-//     {
-//         alert("Please enter a search query.");
-//     }
-// }
-
-// script.js
-
-
-function performGoogleSearch() {
-    // Retrieve the input value
-    var inputValue = document.getElementById("searchInput").value;
-
-    // Check if the input value is not empty
-    if (inputValue.trim() !== "") {
-        // Construct the Google search URL
-        var googleSearchURL = "https://www.google.com/search?q=" + encodeURIComponent(inputValue);
-
-        // Open the Google search in a new tab/window
-        window.open(googleSearchURL, "_blank");
-    } else {
-        // Handle empty search input (optional)
-        alert("Please enter a search query.");
-    }
-}
-
-
-function handleKeyPress(event) {
-    // Check if the key pressed is 'Enter'
-    if (event.key === 'Enter') {
-        // Call the performGoogleSearch function when Enter is pressed
-        performGoogleSearch();
+	        var googleSearchURL = "https://www.google.com/search?q=" + encodeURIComponent(inputValue);
+	        window.open(googleSearchURL, "_blank");
+	    } 
+	    else 
+	    {
+	        alert("Please enter a search query.");
+	    }
     }
 }
