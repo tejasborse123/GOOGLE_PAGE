50699294310428d8dc12eb08165dedb3cdf9e755
Tejas Borse <borsetejas764@gmail.com>
2024-01-07 19:47:27 +0530

    Input Clear after Search

--- data.js
+++ data.js
@@ -3,11 +3,12 @@ function handleKeyPress(event)
     if (event.key === 'Enter') 
     {
         var inputValue = document.getElementById("searchInput").value;
+        var inputField = document.getElementById("searchInput");
 
 	    if (inputValue.trim() !== "") 
 	    {
-
 	        var googleSearchURL = "https://www.google.com/search?q=" + encodeURIComponent(inputValue);
+	        inputField.value = "";
 	        window.open(googleSearchURL, "_blank");
 	    } 
 	    else 
